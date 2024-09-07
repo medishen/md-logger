0be52f24bcf3eab2786abf1215c484fdbe73963d
mahdi <mahdimamashli1383@gmail.com>
2024-09-07 12:43:40 +0330

    chore: The logger class was removed along with its test due to its necessity.

--- test/integration/logger.spec.ts
+++ test/integration/logger.spec.ts
@@ -1,35 +1,32 @@
 import assert from "node:assert";
 import { afterEach, beforeEach, describe, test } from "node:test";
 import { Factory } from "../../lib/logger/Factory";
-import { Logger } from "../../lib/logger/index";
 import * as fs from "fs";
 import path from "node:path";
 const logFilePath = path.resolve(__dirname, "../../logs/app.log");
 describe("MAIN LOGGER", () => {
-  let factory: Factory;
-  let logger: Logger;
+  let logger: Factory;
 
   beforeEach(() => {
     // Initialize the factory and logger before each test
-    factory = new Factory({
+    logger = new Factory({
       file: "app.log",
       level: "debug",
       transports: ["console", "file"],
       jsonFormat: true,
       timestampFormat: "locale",
     });
-    logger = factory.log();
   });
   afterEach(async () => {
     // Close factory and clean up files after each test
-    await factory.close();
+    await logger.close();
     fs.unlink(logFilePath, (err) => {
       if (err) throw err;
     });
   });
   test("Logger should be initialized with correct transports", () => {
-    assert.strictEqual(factory["consoleTransport"] !== undefined, true);
-    assert.strictEqual(factory["fileTransport"] !== undefined, true);
+    assert.strictEqual(logger["consoleTransport"] !== undefined, true);
+    assert.strictEqual(logger["fileTransport"] !== undefined, true);
   });
   test("Logger should log messages to console", () => {
     // Capture console output
@@ -39,7 +36,7 @@ describe("MAIN LOGGER", () => {
       consoleOutput += message;
     };
 
-    logger.info("hi bro");
+    logger.log("hi bro", "info");
 
     // Allow some time for logging to occur
     setImmediate(() => {
