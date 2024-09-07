10c59e9e5c25aeb4af80714f62914faa5469a0d4
mahdi <mahdimamashli1383@gmail.com>
2024-09-07 15:42:20 +0330

    chore: release version 1.0.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,15 +12,15 @@ VERSION=$(node -p "require('./package.json').version")
 # Read the changelog content
 CHANGELOG_CONTENT=$(cat docs/CHANGELOG.md)
 
-# Create a Git commit for the release
-git add .
-git commit -m "chore: release version ${VERSION}"
+
 
 # Create a Git tag with changelog content
 git tag -a "v${VERSION}" -m "Release ${VERSION}: ${CHANGELOG_CONTENT}"
 
-# Push the changes and tags to the remote repository
-git push origin main --tags
-
 # Publish the package
-npm publish
+npm publish --access public
+# Push the changes and tags to the remote repository
+# Create a Git commit for the release
+git add .
+git commit -m "chore: release version ${VERSION}"
+git push origin main --tags
